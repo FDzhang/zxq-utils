3e52352d138d1d925e12c50d8075964e9a46caf4
zhangxinqiang <1933581361@qq.com>
2020-09-02 09:47:52 +0800

    demo of springboot admin server
    测试 springboot admin server

---
+++ demo-admin-server/src/main/java/com/fd/demo/demoadminserver/AdminServerApplication.java
@@ -0,0 +1,19 @@
+package com.fd.demo.demoadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2020/8/28 17:54
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
---
+++ time-consume/src/test/java/com/fd/demo/Test1.java
@@ -0,0 +1,16 @@
+package com.fd.demo;
+
+import org.junit.Test;
+
+/**
+ * @author ：zxq
+ * @date ：Created in 2020/9/1 9:36
+ */
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+
+    }
+}
