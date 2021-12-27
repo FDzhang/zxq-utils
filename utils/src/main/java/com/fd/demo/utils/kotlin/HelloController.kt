9d0714371c044b3567f24172694c9013700ef127
zhangxinqiang <1933581361@qq.com>
2021-12-27 19:00:00 +0800

    kotlin 相关移除

--- utils/src/main/java/com/fd/demo/utils/kotlin/HelloController.kt
+++ utils/src/main/java/com/fd/demo/utils/kotlin/HelloController.kt
@@ -1,28 +1,28 @@
-package com.fd.demo.utils.kotlin
-
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-/**
- * @author     ：zxq
- * @date       ：Created in 2021/1/14 17:29
- */
-@RestController
-@RequestMapping
-class helloController(val helloService: HelloService) {
-    @GetMapping("/hello")
-    fun helloKotlin(): String {
-        return "hello kotlin"
-    }
-
-    @GetMapping("/hello-service")
-    fun helloKotlinService(): String {
-        return helloService.getHello()
-    }
-
-    @GetMapping("hello-dto")
-    fun helloDTO():HelloDTO{
-        return HelloDTO("hello kotlin from the dto")
-    }
-}
+//package com.fd.demo.utils.kotlin
+//
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
+//
+///**
+// * @author     ：zxq
+// * @date       ：Created in 2021/1/14 17:29
+// */
+//@RestController
+//@RequestMapping
+//class helloController(val helloService: HelloService) {
+//    @GetMapping("/hello")
+//    fun helloKotlin(): String {
+//        return "hello kotlin"
+//    }
+//
+//    @GetMapping("/hello-service")
+//    fun helloKotlinService(): String {
+//        return helloService.getHello()
+//    }
+//
+//    @GetMapping("hello-dto")
+//    fun helloDTO():HelloDTO{
+//        return HelloDTO("hello kotlin from the dto")
+//    }
+//}
--- utils/src/main/java/com/fd/demo/utils/kotlin/KotlinTest.kt
+++ utils/src/main/java/com/fd/demo/utils/kotlin/KotlinTest.kt
@@ -1,7 +1,7 @@
-package com.fd.demo.utils.kotlin
-
-/**
- * @author     ：zxq
- * @date       ：Created in 2021/1/14 17:25
- */
-
+//package com.fd.demo.utils.kotlin
+//
+///**
+// * @author     ：zxq
+// * @date       ：Created in 2021/1/14 17:25
+// */
+//
